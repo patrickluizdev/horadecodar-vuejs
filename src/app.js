0048da35d68bbe9732b41e71ff5c9196ff0cca57
patrickluizdev <patrickluizjf@hotmail.com>
2023-02-08 18:06:37 -0300

    Refactor

--- src/app.js
+++ src/app.js
@@ -3,10 +3,22 @@ const MyNameApp = {
         return{
             name: "Patrick",
             idade: "30",
-            message: "Hello Vue;"
+            message: "Hello Vue",
+            input_name: ""
         }
+    },
+
+    method: {
+        submitForm(e) {
+            e.preventDefault();
+            console.log(this.input_name)
+        }
+    
     }
 }
+
+
+
 
 
 Vue.createApp(MyNameApp).mount("#app");
