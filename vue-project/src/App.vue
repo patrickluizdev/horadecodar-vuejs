4b606ef838e773b853b992ccf4fb6e9d485611b1
patrickluizdev <patrickluizjf@hotmail.com>
2023-02-08 22:27:51 -0300

    Feat

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <PrimeiroComponente/>
+  <div>
+    <PrimeiroComponente/>
+    <LifeCycle/>
+  </div>
 </template>
 
 <script>
 
-    import PrimeiroComponente from './components/PrimeiroComponente.vue';
+    import PrimeiroComponente from './components/PrimeiroComponente.vue'
 
-    import LifeCycle from './components/'
+    import LifeCycle from './components/LifeCycle.vue'
 
 
     export default{
       name: 'App',
       components: {
-        PrimeiroComponente
+        PrimeiroComponente,
+        LifeCycle
       }
     }
 
--- vue-project/src/components/LifeCycle.vue
+++ vue-project/src/components/LifeCycle.vue
@@ -1,14 +1,22 @@
 <template>
-    <h1>Meu nome é: {{nome}}</h1>
+    <h1>Meu nome é: {{name}}</h1>
 </template>
 
 <script>
 export default {
-        name: 'LifeCycle',
-        data(){
-            return{
-                name: 'Ainda não Sei'
-            }
+    name: 'LifeCycle',
+    data(){
+        return{
+            name: 'Ainda não Sei'
         }
+    },            
+    created(){
+        setTimeout(() => {
+        this.name = 'Patrick'; 
+        }, 2000);
+    },
+    mounted(){
+        this.name = 'Teste'
+    }
 }
 </script>
