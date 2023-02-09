e53b67b137c1ab76d29456e5750035d208806fed
patrickluizdev <patrickluizjf@hotmail.com>
2023-02-09 20:29:08 -0300

    Feat: condição para variação do nome do botão atravez do status

--- vue-project/src/components/Pessoa.vue
+++ vue-project/src/components/Pessoa.vue
@@ -4,7 +4,7 @@
         <p v-else="tem_nome">huehue</p>
         <p> Acesse o Link <a v-bind:href="link_nome" target="_blank">{{link_nome}}</a> clique aqui </p>
 
-        <button @click="showname">Mostrar</button>
+        <button @click="showname">{{text_button}}</button>
 
         <p>Para acessar clique aqui <a :href="meu_link" target="_blank">Clique Aqui</a></p>
     </div>
@@ -22,13 +22,19 @@ export default {
             name: 'Teste',
             tem_nome: true,
             link_nome: 'https://github.com/',
-            meu_link: 'https://github.com/'
+            meu_link: 'https://github.com/',
+            text_button: 'Mostrar Nome'
         }
     },
 
     methods: {
         showname(){
             this.tem_nome = !this.tem_nome
+            if(!this.tem_nome){
+                this.text_button = 'Mostrar Nome'
+            }else{
+                this.text_button = 'Esconder Nome'
+            }
         }
     }
 }
